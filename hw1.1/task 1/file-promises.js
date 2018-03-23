4fe6c60b8057722e19080f7845eb406e6a42d069
Dmitry <zhestkovda@gmail.com>
2018-03-23 11:45:58 +0500

    Update file-promises.js

--- hw1.1/task 1/file-promises.js
+++ hw1.1/task 1/file-promises.js
@@ -2,25 +2,27 @@ const fs = require('fs');
 const conf = {encoding:'utf8'};
 
 const read = (file) => {
-	return new Promise((done,fail) =>     
-		{
-			fs.readFile(file, conf, (err,content) => {
-				if(err) 
-					{fail(err);}
-				else
-					{done(content);}
-			});
-		})
+	return new Promise((done,fail) => {
+		fs.readFile(file, conf, (err,content) => {
+			if(err){
+				fail(err);
+			}
+			else{
+				done(content);
+			}
+		});
+	})
 }
 
 const write = (file, data) => {
-	return new Promise((done,fail) => 
-	{
+	return new Promise((done,fail) => {
 		fs.writeFile(file, data, conf, err => {
-		if(err)
-			{fail(err);}
-		else
-			{done(file);}
+			if(err){
+				fail(err);
+			}
+			else{
+				done(file);
+			}
 		});
 	})
 }
