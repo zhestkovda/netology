8ff24dd2e5e316b3c18deae973d877f4f67c57d5
Dmitry <zhestkovda@gmail.com>
2018-04-05 09:53:14 +0500

    Update index.js

--- hw2.1/index.js
+++ hw2.1/index.js
@@ -10,24 +10,46 @@ const conf 	   = {encoding:'utf8'};
 const url  		= 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160723T183155Z.f2a3339517e26a3c.d86d2dc91f2e374351379bb3fe371985273278df';
 const port		= 3000;
 
-var rusText;
-var enText;
-
 //console.log(url+ '&' + postData);
 
-function handler1(response) {
-	let data = '';
-	response.on('data', function (chunk) {
-		data += chunk;
-	});
-	response.on('end', function () {
-		enText = JSON.parse(data).text[0];
-		//console.log(enText);
-		return enText
-	});
+function translate(rusText){
+	return  new Promise((done,fail) => {
+		const postData = querystring.stringify({
+		  'text': rusText,
+		  'lang': 'ru-en'
+		});
+		const request = https.request(url + '&' + postData);
+		request.on('response', (response) => {
+			let data = '';
+			response.on('data', function (chunk) {
+				data += chunk;
+			});
+			response.on('end', function () {
+				console.log(data);
+				done(JSON.parse(data).text[0]);
+			});
+		});
+		request.on('error', (e) => {
+			fail(e);
+		})
+		request.end();
+	})
 }
 
-
+function sendTranslation(res, data){
+	return new Promise ((done, fail) => {
+		try{
+			res.writeHeader(200, {'Content-Type': 'application/x-www-form-urlencoded'}); 
+			res.write(data);
+		    res.end();
+		    done('Перевод был успешно отправлен')
+		}
+		catch(ex)
+		{
+			fail(ex);
+		}
+	})
+}
 
 const server = http.createServer(function (req, res) {
 	fs.readFile('./index.html', conf, function (err, html){
@@ -37,7 +59,7 @@ const server = http.createServer(function (req, res) {
 	    else{
 			res.writeHeader(200, {"Content-Type": "text/html"}); 
 			res.write(html); 
-			res.end(); //end the response
+			//res.end(); //end the response
 	    }
 	});
 });
@@ -51,27 +73,11 @@ server.listen(port); //the server object listens on port 8080
 function handler2(req, res) {
 	var q = urlpack.parse(req.url, true);
 	var qdata = q.query;
-	if(Object.keys(qdata).length){   // если есть строка для перевода
-		const postData = querystring.stringify({
-		  'text': qdata.content,
-		  'lang': 'ru-en'
-		});
-		const request = https.request(url+ '&' + postData);
-		request.on('response', handler1);
-		request.end();
-
-		console.log(enText);
-		//res.writeHeader(200, {"Content-Type": "text/html"}); 
-		res.write(querystring.stringify(enText));
-		//setTimeout(function() { 	
-	    //  	res.end();
-	    //}, 2000);
+	if(Object.keys(qdata).length){   // если есть строка для перевода	
+		translate(qdata.content)
+		.then(data => sendTranslation(res, data))
+		.catch(error => console.error(error))
 	}
-
-	
-	//res.writeHead(200, 'OK', {'Content-Type': 'text/html'});
-	//res.write(qdata.content);
-	//res.end();
-
 }
+
 
